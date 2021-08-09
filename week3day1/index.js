a814b9501375aee496f6d2e60833fadbe5b49336
PlayZone <sergeyrushenko98@gmail.com>
2021-08-09 17:40:29 +0300

    block2

---
+++ week3day1/index.js
@@ -0,0 +1,14 @@
+let error = 'error 403!!!'
+// alert(error)
+//
+let score = prompt ('Оцените правильность выполнения задания от 1 до 10', )
+// alert('Моя оценка ' + score + ' баллов')
+
+let test1 = true
+let test2 = false
+let test3 = 'null'
+let test4 = 0
+let test5 = 3
+let test6 = -1
+let test7 = -0
+console.log(!!test1, !!test2, !!test3, !!test4, !!test5, !!test6, !!test7)
